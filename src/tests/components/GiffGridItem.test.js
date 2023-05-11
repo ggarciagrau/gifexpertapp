ef75f71e9d8a6067a963961e3f927de4f9520bc0
Gerard Garcia Grau <gerardgarciagrau2@gmail.com>
2023-05-11 20:32:29 +0200

    component test implementation

--- src/tests/components/GiffGridItem.test.js
+++ src/tests/components/GiffGridItem.test.js
@@ -1,19 +1,45 @@
+import { render, screen } from "@testing-library/react";
 import { GifGridItem } from "../../components";
-import { render } from "@testing-library/react";
 
 describe("GifGridItem tests", () => {
 
     const title = "Google";
-    const url = "https://google.es";
+    const url = "https://google.es/";
 
     test("Renders", () => {
 
         const { container } = render(
             <GifGridItem
-                title="Google"
-                url="https://google.es"
+                title={title}
+                url={url}
             />);
 
         expect(container).toMatchSnapshot();
+    });
+
+    test("Must display the image and the ALT attribute", () => {
+
+        render(
+            <GifGridItem
+                title={title}
+                url={url}
+            />);
+        // screen.debug();
+
+        const { src, alt } = screen.getByRole('img');
+
+        expect(src).toBe(url);
+        expect(alt).toBe(title);
+    });
+
+    test("Must display the title", () => {
+
+        render(
+            <GifGridItem
+                title={title}
+                url={url}
+            />);
+
+        expect(screen.getByText(title)).toBeTruthy();
     });
 });
