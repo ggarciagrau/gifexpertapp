fe06dc06f4704e8b2cc37e0530d1ba95bcd88ceb
Gerard Garcia Grau <gerardgarciagrau2@gmail.com>
2023-05-11 21:51:11 +0200

    jest functions - addcategories tests

--- src/tests/components/AddCategory.test.jsx
+++ src/tests/components/AddCategory.test.jsx
@@ -2,9 +2,14 @@ import { render, fireEvent, screen } from "@testing-library/react";
 import { AddCategory } from "../../components";
 
 describe("AddCategory tests", () => {
-
+    
     const onNewCategory = () => { };
+    const onNewCategoryMock = jest.fn();
 
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+    
     test("Renders", () => {
 
         const { container } = render(<AddCategory onNewCategory={onNewCategory} />);
@@ -27,7 +32,7 @@ describe("AddCategory tests", () => {
 
     test("Must call onNewCategory if the input has a value", () => {
         const value = "Hello World";
-        const { container } = render(<AddCategory onNewCategory={onNewCategory}/>);
+        const { container } = render(<AddCategory onNewCategory={onNewCategoryMock}/>);
 
         const form = screen.getByRole('form');
         // const form = container.querySelector('form');
@@ -37,5 +42,23 @@ describe("AddCategory tests", () => {
         fireEvent.submit(form);
 
         expect(input.value).toBe("");
-    })
+        expect(onNewCategoryMock).toHaveBeenCalledTimes(1);
+        expect(onNewCategoryMock).toHaveBeenCalledWith(value);
+    });
+
+    test("Mustn't call onNewCategory if the input hasn't a value", () => {
+
+        const value = "";
+        const { container } = render(<AddCategory onNewCategory={onNewCategoryMock}/>);
+
+        const form = screen.getByRole('form');
+        const input = container.querySelector('input');
+
+        fireEvent.change(input, { target: { value } });
+        fireEvent.submit(form);
+
+        expect(input.value).toBe("");
+        // expect(onNewCategoryMock).toHaveBeenCalledTimes(0);
+        expect(onNewCategoryMock).not.toHaveBeenCalled();
+    });
 });
